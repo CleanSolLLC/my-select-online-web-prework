cca1c2a94a3955d40c8e523572cca508b25361b5
Mark May <mamark90@gmail.com>
2019-04-06 11:20:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
 def my_select(collection)
- # your code here!
+array_even = []
+
+  i = 0
+
+while i < collection.length
+
+    passed_value = collection[i]
+    true_false = yield collection[i]
+    if true_false == true
+      array_even << passed_value
+    puts "#{array_even}"
+  end
+    i+=1
+  end
+  collection
+end
+
+even_array = []
+my_select([1, 2, 3, 4, 5]) do |num|
+   num.even?
 end
